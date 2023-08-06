87cd3b2e0a7f0d88b6cda68cd200089f470c468a
Khant Si Thu <khantsithu.work@gmail.com>
2023-08-06 23:18:09 +0630

    configure env

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,19 +1,22 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
-import { Task } from "./entity/Task"
-import { Category } from "./entity/Category"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
+import { Task } from "./entity/Task";
+import { Category } from "./entity/Category";
+
+// Load environment variables from .env file
+require("dotenv").config();
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "root123",
-    database: "cleaning_app_db",
-    synchronize: true,
-    logging: false,
-    entities: [User, Task, Category],
-    migrations: [User,Task,Category],
-    subscribers: [],
-})
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: process.env.DB_SYNCHRONIZE === "true",
+  logging: process.env.DB_LOGGING === "true",
+  entities: [User, Task, Category],
+  migrations: [User, Task, Category],
+  subscribers: [],
+});
